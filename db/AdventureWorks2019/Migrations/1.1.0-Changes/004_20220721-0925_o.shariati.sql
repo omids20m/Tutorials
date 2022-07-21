1888b5d83534a4482b7a4dff9cfa7d1292cc6fd2
Omid Shariati <omids20m@gmail.com>
2022-07-21 09:27:31 +0430

    added Temp_Table2

---
+++ db/AdventureWorks2019/Migrations/1.1.0-Changes/004_20220721-0925_o.shariati.sql
@@ -0,0 +1,15 @@
+﻿-- <Migration ID="73861a65-bd20-4289-8ee9-33fed28b6eaf" />
+GO
+
+PRINT N'Creating [dbo].[Temp_Table2]'
+GO
+CREATE TABLE [dbo].[Temp_Table2]
+(
+[Id] [int] NOT NULL,
+[Field1] [nchar] (10) NULL
+)
+GO
+PRINT N'Creating primary key [PK_Temp_Table2] on [dbo].[Temp_Table2]'
+GO
+ALTER TABLE [dbo].[Temp_Table2] ADD CONSTRAINT [PK_Temp_Table2] PRIMARY KEY CLUSTERED ([Id])
+GO
